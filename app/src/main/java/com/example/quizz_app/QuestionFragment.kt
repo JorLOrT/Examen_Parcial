7f37a4e03151fcf356cfd9a2d5b3a95d626e8d45
JorLOrT <jortizc@ulasalle.edu.pe>
2024-05-11 16:34:59 -0500

    Creación de los primeros fragments y "setteo" de la navegación.

---
+++ app/src/main/java/com/example/quizz_app/QuestionFragment.kt
@@ -0,0 +1,12 @@
+package com.example.quizz_app
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class QuestionFragment : Fragment(R.layout.fragment_question) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/example/quizz_app/WelcomeFragment.kt
@@ -0,0 +1,12 @@
+package com.example.quizz_app
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
