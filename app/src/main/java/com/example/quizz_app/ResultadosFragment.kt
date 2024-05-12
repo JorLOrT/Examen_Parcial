1b408c6b1406029bc1092c960b58a27d2563ba27
JorLOrT <jortizc@ulasalle.edu.pe>
2024-05-12 00:47:41 -0500

    Se añadieron comentarios al ResultadosFragment.

--- app/src/main/java/com/example/quizz_app/ResultadosFragment.kt
+++ app/src/main/java/com/example/quizz_app/ResultadosFragment.kt
@@ -13,20 +13,22 @@ class ResultadosFragment : Fragment(R.layout.fragment_resultados) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // Variable para almacenar el número de aciertos obtenidos
         var aciertos: Int = 0
-
+        // Recupera el número de aciertos del bundle pasado desde el fragmento anterior
         arguments.let {bundle->
           aciertos = bundle?.getInt("aciertos").toString().toInt()
         }
-
+        // Obtiene referencias a los TextViews para mostrar la cantidad de aciertos y el reconocimiento
         val aciertos_tv: TextView = view.findViewById(R.id.cantAciertos)
         val reconocimiento_tv: TextView = view.findViewById(R.id.reconocimiento)
-
+        // Establece el texto de la cantidad de aciertos
         aciertos_tv.text = "${aciertos}/7"
+        // Evalúa el número de aciertos y obtiene el texto de reconocimiento correspondiente
         val reconocimiento: String = evaluar(aciertos)
         reconocimiento_tv.text = reconocimiento
     }
-
+    // Función para evaluar el número de aciertos y obtener el texto de reconocimiento correspondiente
     fun evaluar(aciertos: Int): String {
         return when (aciertos) {
             in 6..7 -> "Eres un verdadero Super Saiyajin!"
